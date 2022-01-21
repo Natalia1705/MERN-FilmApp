da02a366f5ecf4d1c694defa8a0b29b5b36daabe
Natalia <lau.nat.her.her@gmail.com>
2022-01-20 20:07:52 -0600

    fist commit

--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
 const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+
+//mongo conection
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Conexión ok"))
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+
+//routes
+app.use("/api/auth", authRoute);
+
+app.listen(3000, () => {
+  console.log("Server on PORT");
+});
